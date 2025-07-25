6c19ff3caffdfdde5a545f1b747e85e2e59a3be7
xiaoxiaoranxxx <2425940130@qq.com>
2025-07-25 22:12:31 +0800

    优化下poc和pass

--- common/config.go
+++ common/config.go
@@ -7,12 +7,22 @@ var Userdict = map[string][]string{
 	"smb":        {"administrator", "admin", "guest"},
 	"rdp":        {"administrator", "admin", "guest"},
 	"postgresql": {"postgres", "admin"},
-	"ssh":        {"root", "admin"},
+	"ssh":        {"root", "admin", "mysql", "db2cat"},
 	"mongodb":    {"root", "admin"},
 	"oracle":     {"sys", "system", "admin", "oracle", "test", "web", "orcl"},
 }
 
-var Passwords = []string{"123456", "12345678", "admin", "admin123", "root", "", "pass123", "pass@123", "password", "123123", "654321", "111111", "123", "1", "admin@123", "Admin@123", "admin123!@#", "{user}", "{user}1", "{user}111", "{user}123", "{user}@123", "{user}_123", "{user}#123", "{user}@111", "{user}@2019", "{user}@123#4", "P@ssw0rd!", "P@ssw0rd", "Passw0rd", "qwe123", "12345678", "test", "test123", "123qwe", "123qwe!@#", "123456789", "123321", "666666", "a123456.", "123456~a", "123456!a", "000000", "1234567890", "8888888", "!QAZ2wsx", "1qaz2wsx", "abc123", "abc123456", "1qaz@WSX", "a11111", "a12345", "Aa1234", "Aa1234.", "Aa12345", "a123456", "a123123", "Aa123123", "Aa123456", "Aa12345.", "sysadmin", "system", "1qaz!QAZ", "2wsx@WSX", "qwe123!@#", "Aa123456!", "A123456s!", "sa123456", "1q2w3e", "Charge123", "Aa123456789"}
+var Passwords = []string{"123456", "12345678", "admin", "admin123", "root", "",
+	"mysql", "db2cat", "pass123", "pass@123", "password", "123123", "654321", "111111", "123", "1",
+	"admin@123", "Admin@123", "admin123!@#", "{user}", "{user}1", "{user}111", "{user}123", "{user}@123",
+	"{user}_123", "{user}#123", "{user}@111", "{user}@123#4", "P@ssw0rd!", "P@ssw0rd", "{user}@2022", "{user}@2023", "{user}@2024",
+	"Passw0rd", "qwe123", "12345678", "test", "test123", "123qwe", "123qwe!@#", "123456789", "123321", "!QAZ@WSX",
+	"666666", "a123456.", "123456~a", "123456!a", "000000", "1234567890", "8888888", "!QAZ2wsx", "1qaz2wsx", "qwerty", "qwerty123", "qwertyuiop",
+	"abc123", "abc123456", "1qaz@WSX", "a11111", "a12345", "Aa1234", "Aa1234.", "Aa12345", "a123456", "a123123",
+	"Aa123123", "Aa123456", "Aa12345.", "sysadmin", "system", "1qaz!QAZ", "2wsx@WSX", "qwe123!@#", "Aa123456!",
+	"QWE123qwe", "A123456s!", "sa123456", "1q2w3e", "Charge123", "Aa123456789", "Huawei@123", "!QAZ2wsx#EDC", "1qaz@WSX3edc", "1qaz2wsx#EDC",
+}
+
 var PORTList = map[string]int{
 	"ftp":         21,
 	"ssh":         22,
@@ -136,7 +146,7 @@ var (
 )
 
 var (
-	UserAgent     = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/534.50 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/534.50"
+	UserAgent     = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
 	Accept        = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
 	DnsLog        bool
 	PocNum        int
